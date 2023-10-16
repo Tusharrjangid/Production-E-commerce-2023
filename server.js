a25e22e07cbdc2b5cbabdd2ffdb56cadc84b8eb7
Tusharrjangid <71558301+Tusharrjangid@users.noreply.github.com>
2023-10-16 19:35:11 +0530

    second update fireurl
    fileurl

--- server.js
+++ server.js
@@ -1,38 +1,42 @@
 import express from "express";
 import colors from "colors";
-import dotenv from "dotenv";
 import morgan from "morgan";
-import ConnectDb from "./config/db.js";
-import authRoute from "./routes/authRoute.js";
-import categoryRoute from "./routes/categoryRoute.js";
-import productRoute from "./routes/productRoute.js";
-import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import UserRoute from "./route/userRoute.js";
+import AdminRoute from "./route/adminRoute.js";
+import DoctorRoute from "./route/doctorRoute.js";
+import { fileURLToPath } from "url"; // Import fileURLToPath
+import { dirname } from "path"; // Import dirname
 import path from "path";
 
 dotenv.config();
 
-ConnectDb();
+const __filename = fileURLToPath(import.meta.url); // Get the current module's filename
+const __dirname = dirname(__filename); // Get the current module's directory name
+
+connectDB();
 
 const app = express();
 
-app.use(cors());
-app.use(morgan("dev"));
 app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("/api/v1/user", UserRoute);
+app.use("/api/v1/admin", AdminRoute);
+app.use("/api/v1/doctor", DoctorRoute);
+
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/category", categoryRoute);
-app.use("/api/v1/product", productRoute);
-
-app.use("*", function (req, res) {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-const PORT = process.env.PORT || 8080;
+const port = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log(
-    `Server is running in ${process.env.DEV_MODE} on port ${process.env.PORT}`
-      .bgCyan.white
+    `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+      .bgCyan
   );
 });
